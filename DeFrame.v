301be17e23889af99215b7a325b4016bd2e9d50b
kondajoshitha07-droid <konda.joshitha07@gmail.com>
2026-08-09 21:17:09 +0530

    Add files via upload

---
+++ DeFrame.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/06/2026 09:38:30 AM
+// Design Name: 
+// Module Name: DeFrame
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module DeFrame(
+    input wire  [10:0]  data_parll,     //  Data frame passed from the sipo unit.
+    input wire          received_flag,
+
+    output reg          parity_bit,     //  The parity bit separated from the data frame.
+    output reg          start_bit,      //  The Start bit separated from the data frame.
+    output reg          stop_bit,       //  The Stop bit separated from the data frame.
+    output reg          done_flag,      //  Indicates that the data is recieved and ready for another data packet.
+    output reg  [7:0]   raw_data        //  The 8-bits data separated from the data frame.
+);
+
+//  Deframing 
+always @(*) 
+begin
+  start_bit       = data_parll[0];
+  raw_data[7:0]   = data_parll[8:1];
+  parity_bit      = data_parll[9];
+  stop_bit        = data_parll[10];
+  done_flag       = received_flag;
+end
+
+endmodule
---
+++ ErrorCheck.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/06/2026 09:37:28 AM
+// Design Name: 
+// Module Name: ErrorCheck
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module ErrorCheck(
+    input wire         reset_n,       //  Active low reset.
+    input wire         received_flag, //  enable from the sipo unit for the flags.
+    input wire         parity_bit,    //  The parity bit from the frame for comparison.
+    input wire         start_bit,     //  The Start bit from the frame for comparison.
+    input wire         stop_bit,      //  The Stop bit from the frame for comparison.
+    input wire  [1:0]  parity_type,   //  Parity type agreed upon by the Tx and Rx units.
+    input wire  [7:0]  raw_data,      //  The 8-bits data separated from the data frame.
+
+    output wire [2:0]  error_flag     //  {stop_flag,start_flag,parity_flag}
+);
+
+//  Internal
+reg error_parity;
+reg parity_flag;
+reg start_flag;
+reg stop_flag;
+
+//  Encoding for types of the parity
+localparam ODD        = 2'b01,
+           EVEN       = 2'b10;
+
+//  Parity Check logic
+always @(*) 
+begin
+  case (parity_type)
+    ODD:     error_parity = (^raw_data)? 1'b0 : 1'b1;
+    EVEN:    error_parity = (^raw_data)? 1'b1 : 1'b0;
+    default: error_parity = 1'b1;
+  endcase
+end
+
+// Error Check logic
+always @(*) begin
+  parity_flag  = (error_parity ^ parity_bit);
+  start_flag   = (start_bit || 1'b0);
+  stop_flag    = ~(stop_bit && 1'b1);
+end
+
+//  Output logic
+assign error_flag = (reset_n && received_flag)? {stop_flag,start_flag,parity_flag} : 3'b0;
+
+endmodule
